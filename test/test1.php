d1f39f516ebdc2d1aa499de6e1f293f8c6407503
netipat <lovconcept@hotmail.com>
2014-09-15 14:05:30 +0700

    ทดสอบ1
    การเพิ่มข้อมูล

--- test/test1.php
+++ test/test1.php
@@ -1,4 +1,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
-echo "สวัสดีครับ";
+$a = 50;
+$b = 100;
+echo "$a+$b <br>";
+echo '$a+$b <br>';
+echo $a+$b.'<br>';
+echo '<font color="red"><h1>สวัสดีครับ</h1> </font>';
 ?>
